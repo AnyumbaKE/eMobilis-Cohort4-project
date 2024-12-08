7b9ab57d4a266f96ff07cedebc25430e5e044dbd
AnyumbaKE <esanyumba@gmail.com>
2024-12-08 15:30:20 +0300

    update

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from listings.choices import price_choices , bedroom_choices , county_choices
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
+def index(request):
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True);
+	return render(request , 'pages/index.html',{'listings' : listings ,
+        'county_choices' : county_choices,
+        'bedroom_choices' : bedroom_choices,
+        'price_choices' : price_choices,
+
+		})
+
+
+def about(request):
+	realtors = Realtor.objects.order_by('-hire_date')
+	mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+	context = {
+	   'realtors' : realtors,
+	   'mvp_realtors' : mvp_realtors
+	}
+	return render(request , 'pages/about.html',context)
